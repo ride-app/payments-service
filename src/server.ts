14017f547af89f33d1a318852e48f1f0d8d139cf
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-11 22:00:38 +0530

    Revert "refactor: try to see if turning trustproxy off will fix connection reset err"
    This reverts commit 317c0134cec438fc233f5fdea634f76ab995e144.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { fastify } from "fastify";
 import { fastifyConnectPlugin } from "@bufbuild/connect-fastify";
 import routes from "./wallet-service/service.js";
 
-const server = fastify();
+const server = fastify({ trustProxy: true });
 
 await server.register(fastifyConnectPlugin, {
 	routes,
