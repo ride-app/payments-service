317c0134cec438fc233f5fdea634f76ab995e144
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-11 21:48:36 +0530

    refactor: try to see if turning trustproxy off will fix connection reset err

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { fastify } from "fastify";
 import { fastifyConnectPlugin } from "@bufbuild/connect-fastify";
 import routes from "./wallet-service/service.js";
 
-const server = fastify({ trustProxy: true });
+const server = fastify();
 
 await server.register(fastifyConnectPlugin, {
 	routes,
