14e095ec3046c9f06d09f675866d39a714ff1555
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-11 23:55:20 +0530

    refactor: override server with plain http handler to debug

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { createServer } from "http";
-import { connectNodeAdapter } from "@bufbuild/connect-node";
-import routes from "./wallet-service/service.js";
+// import { connectNodeAdapter } from "@bufbuild/connect-node";
+// import routes from "./wallet-service/service.js";
 
 const server = createServer(
-	connectNodeAdapter({ routes }) // responds with 404 for other requests
+	(req, res) => {
+		res.write("Hello World!"); // write a response to the client
+		res.end(); // end the response
+	}
+	// connectNodeAdapter({ routes }) // responds with 404 for other requests
 );
 
 export default server;
