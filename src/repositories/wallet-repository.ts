bab05e3cdec05ae6e574a7ed0c87de4ed89ccfa6
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-12 19:04:26 +0530

    chore: delete comment

--- src/repositories/wallet-repository.ts
+++ src/repositories/wallet-repository.ts
@@ -19,36 +19,6 @@ export default class WalletRepository {
 		return this._instance;
 	}
 
-	// async createWallet(uid: string): Promise<Timestamp> {
-	// await this.firestore.runTransaction(async (transaction) => {
-	// 	const walletRef = this.firestore.collection("wallets");
-	// 	const snap = await transaction.get(
-	// 		this.firestore.collection("wallets").doc(uid)
-	// 	);
-
-	// 	if (snap.exists) {
-	// 		throw new ConnectError("Wallet Already Exists", Code.AlreadyExists);
-	// 	}
-
-	// 	const payload = {
-	// 		balance: 0,
-	// 		createdAt: FieldValue.serverTimestamp(),
-	// 		updatedAt: FieldValue.serverTimestamp(),
-	// 	};
-
-	// 	transaction.create(walletRef.doc(uid), payload);
-	// });
-
-	// 	const writeResult = await this.firestore
-	// 		.collection("wallets")
-	// 		.doc(uid)
-	// 		.set({
-	// 			balance: 0,
-	// 		});
-
-	// 	return Timestamp.fromDate(writeResult.writeTime.toDate());
-	// }
-
 	async getWallet(uid: string): Promise<Wallet | undefined> {
 		const snap = await this.firestore.collection("wallets").doc(uid).get();
 
