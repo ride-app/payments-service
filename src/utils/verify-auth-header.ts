1e518982f580851d859edeb9d4249d96e5c12c4f
Debkanchan Samadder <debu.samadder@gmail.com>
2023-07-12 18:04:44 +0530

    chore: try to rebug auth verification

--- src/utils/verify-auth-header.ts
+++ src/utils/verify-auth-header.ts
@@ -6,7 +6,9 @@ const verifyAuthHeader = async (context: HandlerContext): Promise<string> => {
 		if (context.requestHeader.get("authorization").length === 0) {
 			throw new ConnectError("Missing Authorization", Code.Unauthenticated);
 		}
-		const token = context.requestHeader.get("authorization")[0].toString();
+		const token = context.requestHeader.get("authorization").toString();
+
+		console.debug("Token: ", token);
 
 		if (!token.startsWith("Bearer ")) {
 			throw new ConnectError("Invalid Authorization", Code.Unauthenticated);
