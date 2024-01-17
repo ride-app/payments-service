21520e45ebf3563ee48035802ecad999bee9b09c
Debkanchan Samadder <debu.samadder@gmail.com>
2024-01-17 21:41:00 +0530

    chore: upgrade to keyfunc v3

--- api/interceptors/auth_interceptor.go
+++ api/interceptors/auth_interceptor.go
@@ -3,10 +3,9 @@ package interceptors
 import (
 	"context"
 	"errors"
-	"time"
 
 	"connectrpc.com/connect"
-	"github.com/MicahParks/keyfunc/v2"
+	"github.com/MicahParks/keyfunc/v3"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/ride-app/wallet-service/utils/logger"
 )
@@ -14,18 +13,7 @@ import (
 func NewAuthInterceptor(ctx context.Context, log logger.Logger) (*connect.UnaryInterceptorFunc, error) {
 	jwksURI := "https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com"
 
-	options := keyfunc.Options{
-		Ctx: ctx,
-		RefreshErrorHandler: func(err error) {
-			log.WithError(err).Fatal("There was an error with the jwt.Keyfunc")
-		},
-		RefreshInterval:   time.Hour,
-		RefreshRateLimit:  time.Minute * 5,
-		RefreshTimeout:    time.Second * 10,
-		RefreshUnknownKID: true,
-	}
-
-	jwks, err := keyfunc.Get(jwksURI, options)
+	k, err := keyfunc.NewDefault([]string{jwksURI})
 
 	if err != nil {
 		return nil, err
@@ -49,7 +37,7 @@ func NewAuthInterceptor(ctx context.Context, log logger.Logger) (*connect.UnaryI
 					errors.New("invalid token format"),
 				)
 			}
-			token, err := jwt.Parse(req.Header().Get("authorization")[7:], jwks.Keyfunc)
+			token, err := jwt.Parse(req.Header().Get("authorization")[7:], k.Keyfunc)
 
 			if !token.Valid {
 				return nil, connect.NewError(
@@ -58,7 +46,7 @@ func NewAuthInterceptor(ctx context.Context, log logger.Logger) (*connect.UnaryI
 				)
 			}
 
-			req.Header().Set("user_id", token.Claims.(jwt.MapClaims)["user_id"].(string))
+			req.Header().Set("uid", token.Claims.(jwt.MapClaims)["user_id"].(string))
 
 			if err != nil {
 				return nil, connect.NewError(
