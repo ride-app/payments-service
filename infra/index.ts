9159450ebc5002ff681d3d9e708f14ba7912ea41
Debkanchan Samadder <debu.samadder@gmail.com>
2024-02-24 19:58:45 +0530

    ci(cloudbuild): update cloudbuild

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,24 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
 
 const serviceName =
   new pulumi.Config("service").get("name") || pulumi.getProject();
 const location = gcp.config.region || "asia-east1";
+
+const bucket = new gcp.storage.Bucket("bazel-remote-cache", {
+  location, // Replace with desired location if needed.
+  uniformBucketLevelAccess: true,
+});
+
+// IAM policy to allow the specific service account read and write access to the bucket
+const bucketIAMMemberUser = new gcp.storage.BucketIAMMember(
+  "bucketIAMMemberUser",
+  {
+    bucket: bucket.name,
+    role: "roles/storage.objectUser",
+    member: `serviceAccount:${gcp.config.project}@cloudbuild.gserviceaccount.com`,
+  },
+);
 
 const github_connection = gcp.cloudbuildv2.Connection.get(
   "github-connection",
@@ -25,6 +40,7 @@ new gcp.cloudbuild.Trigger("build-trigger", {
     },
   },
   substitutions: {
+    _BAZEL_REMOTE_CACHE_BUCKET: bucket.name,
     _LOG_DEBUG: new pulumi.Config().get("logDebug") ?? "false",
   },
   filename: "cloudbuild.yaml",
